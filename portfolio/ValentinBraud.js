08a4a8673fef3d3cddad0b1cd7c77d341029950e
Valentin Braud <34383968+vbraud@users.noreply.github.com>
2020-05-08 05:42:17 +0200

    1.0

---
+++ portfolio/ValentinBraud.js
@@ -0,0 +1,20 @@
+var image_portfolio = ['portfolio/FINI/ALL N.png','portfolio/FINI/paix.png', 'portfolio/FINI/DF.png', 'portfolio/FINI/Future noir.png', 'portfolio/FINI/mort 2.png', 'portfolio/FINI/RAY 2.png', 'portfolio/FINI/MAXOU N.png', 'portfolio/FINI/SAMOURAIL.png', 'portfolio/FINI/Fin du Monde.png', 'portfolio/FINI/BAT.png', 'portfolio/FINI/ONE PLUS MAN.png', 'portfolio/FINI/Bonne mort.png', 'portfolio/FINI/PARTIT EN COUILLE.png'];
+
+let i = 0;
+
+function gauche(){
+    if(i != 0){
+        i--;
+        document.getElementById("img").src = image_portfolio[i];
+    }
+}
+
+function droite(){
+    if(i < image_portfolio.length - 1){
+        i++;
+        document.getElementById("img").src = image_portfolio[i];
+    }
+}
+
+
+
